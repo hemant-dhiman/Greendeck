a7ecbd411be940c28e9333169523b4bca26d1118
HEMANT <hemant.dhiman92@gmail.com>
2020-10-08 12:06:04 +0530

    data base uploaded

--- DataBaseFile/database.py
+++ DataBaseFile/database.py
@@ -1,8 +1,13 @@
 from pymongo import MongoClient
 
+# Making connection
 mongo_obj = MongoClient(
     "mongodb+srv://GreenDeck:greendeck123@cluster0.0wnzx.gcp.mongodb.net/GreenDeck?retryWrites=true&w=majority")
 
+'''a = mongo_obj.list_database_names()
+for i in enumerate(a):
+    print(i)'''
 
 def client():
+    # returning a client
     return mongo_obj
--- app.py
+++ app.py
@@ -2,22 +2,45 @@ from flask import Flask
 import pymongo
 import json
 from bson import json_util
+# importing client
 from DataBaseFile.database import client
+import markdown
+
+mongo_obj = client()
+
+db = mongo_obj["Greendeck_SE_Assignment_Task_1"]
+collection = db["Data"]
 
 '''
-mongo_obj = MongoClient(
-    "mongodb+srv://GreenDeck:greendeck123@cluster0.0wnzx.gcp.mongodb.net/GreenDeck?retryWrites=true&w=majority")
-'''
-mongo_obj = client()
 db = mongo_obj["greendeck"]
 collection = db["data"]
+'''
 
 app_obj = Flask(__name__)
+
+# open the data base file in locally
+with open("DataBaseFile/Greendeck SE Assignment Task 1.json") as file:
+    file_data = json.load(file)
+
+count = mongo_obj["Greendeck_SE_Assignment_Task_1"]["Data"].count_documents({})
+
+if count < 5000:
+    if isinstance(file_data, list):
+        collection.insert_many(file_data)
+    else:
+        collection.insert_one(file_data)
+    print("database Upload done!")
+else:
+    print("data Already uploaded!")
 
 
 @app_obj.route("/")
 def get_data():
-    return "hello api"
+    with open(app_obj.root_path + '/README.md', 'r') as mrkdwn:
+        md = mrkdwn.read()
+        print(md)
+    return markdown.markdown(md)
+    # return 'hello api'
 
 
 @app_obj.route("/database", methods=["GET"])
@@ -26,5 +49,7 @@ def data():
     return json.dumps(dbs, default=json_util.default)
 
 
+'''
 if __name__ == '__main__':
-    app_obj.run(debug=True)
+    #app_obj.run(debug=True)
+'''
