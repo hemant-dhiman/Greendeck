4d9b486fc2f00452b3e1a8881452d84836478775
HEMANT <hemant.dhiman92@gmail.com>
2020-10-12 19:34:20 +0530

    assignment all CRUD operations done. with comments

--- DataBaseFile/database.py
+++ DataBaseFile/database.py
@@ -5,28 +5,7 @@ import json
 mongo_obj = MongoClient(
     "mongodb+srv://GreenDeck:greendeck123@cluster0.0wnzx.gcp.mongodb.net/GreenDeck?retryWrites=true&w=majority")
 
-'''a = mongo_obj.list_database_names()
-for i in enumerate(a):
-    print(i)
-'''
-
 
 def client():
     # returning a client
     return mongo_obj
-
-
-# db = mongo_obj["Greendeck_SE_Assignment_Task_1"]
-# collection = db["Data"]
-
-# dbs = list(collection.find({}))
-
-# print(dbs[0])
-'''
-with open("Greendeck SE Assignment Task 1.json") as file:
-    file_data = json.load(file)
-
-file_data[0]['_id'] = 1
-
-print(file_data[0]['_id'])
-'''
--- app.py
+++ app.py
@@ -6,12 +6,13 @@ import markdown
 # importing client
 from DataBaseFile.database import client
 
-mongo_obj = client()
+mongo_obj = client()  # creating client
 
 db = mongo_obj["Greendeck_SE_Assignment_Task_1"]
 collection = db["Data"]
 
 '''
+# db for test purpose
 db = mongo_obj["greendeck"]
 collection = db["data"]
 '''
@@ -58,28 +59,29 @@ def data():
 # view single entry
 @app_obj.route("/database/id/<int:i_d>/", methods=["GET"])  # get one data in the collection
 def one_data(i_d):
-    dbs = collection.find_one(
+    dbs = collection.find_one(  # check if i_d already exist or not, if not it return None
         {"_id": int(i_d)}
     )
     if dbs is not None:
         return str(dbs)
     else:
-        return {"Message": "Not Found"}, 404
+        return {"Message": "Not Found"}, 404  # returning message with error code
 
 
+# POST creating new task
 @app_obj.route("/newdata", methods=["POST"])
 def new():
     new_data = request.json
     # count the latest id
     ids = collection.count_documents({})
 
-    d = collection.find_one(
+    d = collection.find_one(  # check if new_data["_id"] already exist or not, if not it return None
         {"_id": int(new_data["_id"])}
     )
     if d is None:
-        new_data["_id"] = ids + 1  # give new_entry _id attribute a unique value
+        new_data["_id"] = ids + 1  # give new_entry _id attribute a unique value as id
         collection.insert_one(new_data)
-        return {"Inserted": str(new_data)}, 201
+        return {"Inserted": str(new_data)}, 201  # returning code with success message
     else:
         return {"Message": "Id Already exist " + str(d)}
 
@@ -87,29 +89,30 @@ def new():
 # update
 @app_obj.route("/update/id/<int:i_d>/<string:name>", methods=["PUT"])
 def data_update(i_d, name):
-    d1 = collection.find_one(
+    d1 = collection.find_one(  # check if i_d already exist or not, if not it return None
         {"_id": int(i_d)}
     )
-    if d1 is not None:
-        d = collection.update_one({"_id": int(i_d)}, {"$set": {"name": str(name)}})
-        return {"Message": "updated " + str(d)}, 200
+    if d1 is not None:  # if i_d entry in database exist than only it will update the name attribute
+        d = collection.update_one({"_id": int(i_d)}, {"$set": {"name": str(name)}})  # update query (mongodb)
+        return {"Message": "updated " + str(d)}, 200  # return the success message with code
     else:
-        return {"Message": "id {} does't exist!".format(i_d)}
+        return {"Message": "id {} does't exist!".format(i_d)}  # returning simple message does't exist
 
 
 # delete
 @app_obj.route("/remove/id/<int:i_d>", methods=["DELETE"])
 def data_remove(i_d):
-    d = collection.find_one(
+    d = collection.find_one(  # check if i_d already exist or not, if not it return None
         {"_id": int(i_d)}
     )
     if d is not None:
-        collection.remove({"_id": int(i_d)})
-        return {"Message": "Deleted " + str(d)}, 200
+        collection.remove({"_id": int(i_d)})  # after checking it will perform operation of DELETION
+        return {"Message": "Deleted " + str(d)}, 200  # return success code with message
     else:
         return {"Message": "id {} does't exist!".format(i_d)}
 
 
 if __name__ == '__main__':
     print("Application started!")
+    # run command for flask application with host and port number with debug mode on
     app_obj.run(host="0.0.0.0", port=5000, debug=True)
